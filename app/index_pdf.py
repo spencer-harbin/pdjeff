5c81ae2a1d1d2ebdff3f55f290fad7fba9a9f72e
spencer-harbin <sdh7301@gmail.com>
2024-09-17 22:09:45 -0400

    add basic flask routes

---
+++ app/index_pdf.py
@@ -0,0 +1,19 @@
+import glob 
+import pymupdf as pym 
+
+class PdfIndex:
+    def __init__(self, local_file_path):
+        self.local_file_path = local_file_path
+
+    def extract_text_from_pdf(self):
+        text = ""
+        doc = pym.open(self.local_file_path)
+        for page in doc:
+            text += page.get_text()
+        
+        doc.close()
+
+        return text
+    
+    def index_pdf_contents(self):
+        pass
