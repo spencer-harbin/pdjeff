89ec5c192e11e90c2e7a80e306c8c731e12aa660
spencer-harbin <sdh7301@gmail.com>
2024-09-24 16:14:52 -0400

    accept user input

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
+import os 
+import glob
 import pymupdf as pym 
 import mmh3
 
-file_path = '/Users/spencer/Documents/email_papers_script/sample_pdfs/test.pdf'
+use_default_or_custom = input('Would you prefer to use the default (d) or custom (c) file path? > ')
+if use_default_or_custom == 'd':
+    all_files = glob.glob('/Users/spencer/Documents/email_papers_script/sample_pdfs/*.pdf')
+elif use_default_or_custom == 'c':
+    all_files = input('Enter the full file path. >')
+else:
+    print('Unrecognized input. Please try again with c or d.')
+    exit(0)
 
 index = {}
+for file_path in all_files:
+    doc = pym.open(file_path)
+    file_size = os.stat(file_path).st_size
+    pdf_id = mmh3.hash(str(file_size))
 
-doc = pym.open(file_path)
+    for page in doc:
+        pagenum = page.number
+        text = ''
+        text += page.get_text()
+        text_list = text.split(' ')
 
-# update this later to use file size or something instead of just file path
-pdf_id = mmh3.hash(file_path)
+        for word in text_list: 
+            if not index.get(word):
+                index[word] = {'pdf_id':pdf_id, 'page_id':[pagenum]}
+            else:
+                index[word]['pdf_id'] = pdf_id
+                if not pagenum in index[word]['page_id']: index[word]['page_id'].append(pagenum)
 
-for page in doc:
-    pagenum = page.number
-    text = ''
-    text += page.get_text()
-    text_list = text.split(' ')
-
-    for word in text_list: 
-        if not index.get(word):
-            index[word] = {'pdf_id':pdf_id, 'page_id':[pagenum]}
-        else:
-            index[word]['pdf_id'] = pdf_id
-            if not pagenum in index[word]['page_id']: index[word]['page_id'].append(pagenum)
-
-print(index)
-
-# I don't want to do this today
+# print(index)
